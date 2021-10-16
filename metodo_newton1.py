6ac0b3907ce21b45fe4864636e64d8c328fbafed
Goto <mateusgoto01@gmail.com>
2021-10-16 15:04:58 -0300

    [ADDED] Metodo de Newton para sistemas não lineares

--- metodo_newton1.py
+++ metodo_newton1.py
@@ -1,3 +1,6 @@
+#Metodo de newton para sistemas não lineares
+#By Goto
+from __future__ import division
 import math
 import numpy as np
 
@@ -8,6 +11,7 @@ def f(x):
     y[1] = ((x[0])**2) - 81*(x[1] + 0.1)**2 + math.sin(x[2]) + 1.06
     y[2] = math.e**(-x[0]*x[1]) + 20*x[2] + ((10*math.pi - 3)/3)
     return y
+
 
 def jf(x):
     j = np.zeros((3, 3))
@@ -26,5 +30,19 @@ def jf(x):
 
     return j
 
+x_old = 0 #vai aguarda o antigo valor da raiz
+x = np.array([0.1, 0.1, -0.1]) #começo do chute e o valor a ser testado
 
-
+k = 0 #contador de iteracoes
+max_int  = 50 #numero maximo de interacoes
+E1 = 10**(-4) #erro
+while k < 50:
+    k += 1
+    delta = np.dot(np.linalg.inv(jf(x)), f(x))
+    x_old = x
+    x = x - delta
+    if math.fabs(np.linalg.norm(x_old) - np.linalg.norm(x)) <= E1:
+        print("Método de Newton para Sistema não lineares")
+        print(x)
+        print("numero de iterações: ", k)
+        break
