2aaaf8c88825514a9d0b92a852563395293916d6
Goto <mateusgoto01@gmail.com>
2021-10-18 18:18:32 -0300

    [NEW] Metodo de Newton Discreta por diferenças finitas

--- metodo_newtonDiscreto1.py
+++ metodo_newtonDiscreto1.py
@@ -13,35 +13,28 @@ def f(x):
     return y
 
 
-def jf(x):
-    j = np.zeros((3, 3))
-    j[0, 0] = 3
-    j[0, 1] = x[2]*math.sin(x[1]*x[2])
-    j[0, 2] = x[1] * math.sin(x[1] * x[2])
+def jf(x, h):
+    J = np.zeros((3, 3))
+    for j in range(3):
+        for k in range(3):
+            J[j][k] = (f(x + h * np.eye(3)[:, j])[k] - f(x)[k]) / h
 
-    j[1, 0] = 2*x[0]
-    j[1, 1] = -162*x[1] - 16.2
-    j[1, 2] = math.cos(x[2])
-
-    j[2, 0] = -x[1]*(math.e**(-x[0] * x[1]))
-    j[2, 1] = -x[0] * (math.e ** (-x[0] * x[1]))
-    j[2, 2] = 20
-
-    return j
+    return J
 
 x_old = 0 #vai aguarda o antigo valor da raiz
 x = np.array([0.1, 0.1, -0.1]) #começo do chute e o valor a ser testado
 
+h = 10**(-7)
 k = 0 #contador de iteracoes
 max_int  = 50 #numero maximo de interacoes
-E1 = 10**(-6) #erro
+E1 = 10**(-9) #erro
 while k < 50:
     k += 1
-    delta = np.dot(np.linalg.inv(jf(x)), f(x))
+    delta = np.dot(np.linalg.inv(jf(x, h)), f(x))
     x_old = x
     x = x - delta
     if math.fabs(np.linalg.norm(x_old) - np.linalg.norm(x)) <= E1:
-        print("Método de Newton para Sistema não lineares")
+        print("Método de Newton Discreto por diferenças finitas para Sistema não lineares")
         print(x)
         print("numero de iterações: ", k)
         break
